1784c3db35c556d7271787e1d1db7f65420ec0f1
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-01-01 13:21:20 +0100

    Define macros sind jetzt in ihrer eigenen Datei

---
+++ Compiler/src/visualizer/constants.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#define FOOTER_HEIGHT 1
+#define HEADER_HEIGHT 1
+#define MSG_BUS_WIDTH 35
+
+#define COLOR_GREY 50
+#define COLOR_DARK_GREY 51
+
+#define STD_COLOR_PAIR 1
+#define HEADER_COLOR_PAIR 99
+#define FOOTER_COLOR_PAIR 98
+#define MSG_BUS_COLOR_PAIR 97
