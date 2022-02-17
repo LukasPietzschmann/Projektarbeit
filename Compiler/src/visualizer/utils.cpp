f54938c44b52d1e0e2d895d194a200b2d1cd9521
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-02-17 08:36:54 +0100

    Auskommentierten Code entfernt

--- Compiler/src/visualizer/utils.cpp
+++ Compiler/src/visualizer/utils.cpp
@@ -6,21 +6,3 @@ void center_text_hor(WINDOW* window, const CH::str& str, uint32_t y_coordinate) 
 	mvwaddnstr(window, y_coordinate, x_coordinate, &str.elems[0], *str);
 	wnoutrefresh(window);
 }
-
-/*CH::str expr_to_str(const Expr& expr) {
-	const auto& id_or_error = oper_store::the().get_id_from_oper(expr(oper_));
-	const CH::str& id_str = CH::str(std::to_string(*id_or_error));
-	const CH::str& id_prefix = id_or_error.has_value() ? (id_str + ": ") : "?: ";
-
-	CH::str result;
-
-	if(expr(beg_) == expr(end_))
-		result = id_prefix + " " + expr(to_str_);
-	else
-		result = id_prefix + " " + get_scanned_str_for_expr(expr) + expr(to_str_from_currpart_);
-
-	if(*result > REPLACE_WITH_ID_THRESHOLD && id_or_error.has_value())
-		result = "ID: " + id_str;
-
-	return result;
-}*/
--- Compiler/src/visualizer/utils.hpp
+++ Compiler/src/visualizer/utils.hpp
@@ -4,9 +4,5 @@
 #include <ncurses.h>
 
 #include "../libCH/seq.ch"
-// #include "../flxc/data.h"
-
-#include "archive.hpp"
 
 void center_text_hor(WINDOW* window, const CH::str& str, uint32_t y_coordinate);
-// CH::str expr_to_str(const Expr& expr);
